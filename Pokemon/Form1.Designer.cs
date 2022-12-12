d032c40227ce2fee9be40f8e9db416d77a360226
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-12 15:51:45 -0300

    busqueda avanzada

--- Pokemon/Form1.Designer.cs
+++ Pokemon/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.pbPokemon = new System.Windows.Forms.PictureBox();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnBorrarLogica = new System.Windows.Forms.Button();
+            this.lbFiltro = new System.Windows.Forms.Label();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.btnBusqueda = new System.Windows.Forms.Button();
+            this.cmbCampo = new System.Windows.Forms.ComboBox();
+            this.cmbCriterio = new System.Windows.Forms.ComboBox();
+            this.txtBusquedaAvanzada = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPokemon)).BeginInit();
             this.SuspendLayout();
@@ -60,7 +68,7 @@
             // btnAgregar
             // 
             this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnAgregar.Location = new System.Drawing.Point(23, 330);
+            this.btnAgregar.Location = new System.Drawing.Point(23, 388);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(126, 50);
             this.btnAgregar.TabIndex = 2;
@@ -71,7 +79,7 @@
             // btnModificar
             // 
             this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnModificar.Location = new System.Drawing.Point(155, 330);
+            this.btnModificar.Location = new System.Drawing.Point(155, 388);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(126, 50);
             this.btnModificar.TabIndex = 3;
@@ -79,11 +87,94 @@
             this.btnModificar.UseVisualStyleBackColor = true;
             this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
             // 
+            // btnBorrar
+            // 
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBorrar.Location = new System.Drawing.Point(287, 388);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(126, 50);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnBorrarLogica
+            // 
+            this.btnBorrarLogica.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBorrarLogica.Location = new System.Drawing.Point(419, 388);
+            this.btnBorrarLogica.Name = "btnBorrarLogica";
+            this.btnBorrarLogica.Size = new System.Drawing.Size(126, 50);
+            this.btnBorrarLogica.TabIndex = 5;
+            this.btnBorrarLogica.Text = "Borrar Logica";
+            this.btnBorrarLogica.UseVisualStyleBackColor = true;
+            this.btnBorrarLogica.Click += new System.EventHandler(this.btnBorrarLogica_Click);
+            // 
+            // lbFiltro
+            // 
+            this.lbFiltro.AutoSize = true;
+            this.lbFiltro.Location = new System.Drawing.Point(20, 39);
+            this.lbFiltro.Name = "lbFiltro";
+            this.lbFiltro.Size = new System.Drawing.Size(29, 13);
+            this.lbFiltro.TabIndex = 6;
+            this.lbFiltro.Text = "Filtro";
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Location = new System.Drawing.Point(55, 36);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(208, 20);
+            this.txtBusqueda.TabIndex = 7;
+            this.txtBusqueda.TextChanged += new System.EventHandler(this.txtBusqueda_TextChanged);
+            // 
+            // btnBusqueda
+            // 
+            this.btnBusqueda.Location = new System.Drawing.Point(452, 351);
+            this.btnBusqueda.Name = "btnBusqueda";
+            this.btnBusqueda.Size = new System.Drawing.Size(75, 23);
+            this.btnBusqueda.TabIndex = 8;
+            this.btnBusqueda.Text = "Buscar";
+            this.btnBusqueda.UseVisualStyleBackColor = true;
+            this.btnBusqueda.Click += new System.EventHandler(this.btnBusqueda_Click);
+            // 
+            // cmbCampo
+            // 
+            this.cmbCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCampo.FormattingEnabled = true;
+            this.cmbCampo.Location = new System.Drawing.Point(198, 352);
+            this.cmbCampo.Name = "cmbCampo";
+            this.cmbCampo.Size = new System.Drawing.Size(121, 21);
+            this.cmbCampo.TabIndex = 9;
+            this.cmbCampo.SelectedIndexChanged += new System.EventHandler(this.cmbCampo_SelectedIndexchanged);
+            // 
+            // cmbCriterio
+            // 
+            this.cmbCriterio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCriterio.FormattingEnabled = true;
+            this.cmbCriterio.Location = new System.Drawing.Point(325, 352);
+            this.cmbCriterio.Name = "cmbCriterio";
+            this.cmbCriterio.Size = new System.Drawing.Size(121, 21);
+            this.cmbCriterio.TabIndex = 10;
+            // 
+            // txtBusquedaAvanzada
+            // 
+            this.txtBusquedaAvanzada.Location = new System.Drawing.Point(23, 353);
+            this.txtBusquedaAvanzada.Name = "txtBusquedaAvanzada";
+            this.txtBusquedaAvanzada.Size = new System.Drawing.Size(169, 20);
+            this.txtBusquedaAvanzada.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBusquedaAvanzada);
+            this.Controls.Add(this.cmbCriterio);
+            this.Controls.Add(this.cmbCampo);
+            this.Controls.Add(this.btnBusqueda);
+            this.Controls.Add(this.txtBusqueda);
+            this.Controls.Add(this.lbFiltro);
+            this.Controls.Add(this.btnBorrarLogica);
+            this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.pbPokemon);
@@ -95,6 +186,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvPokemon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbPokemon)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -104,6 +196,14 @@
         private System.Windows.Forms.PictureBox pbPokemon;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnBorrarLogica;
+        private System.Windows.Forms.Label lbFiltro;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.Button btnBusqueda;
+        private System.Windows.Forms.ComboBox cmbCampo;
+        private System.Windows.Forms.ComboBox cmbCriterio;
+        private System.Windows.Forms.TextBox txtBusquedaAvanzada;
     }
 }
 
--- Pokemon/Form1.cs
+++ Pokemon/Form1.cs
@@ -15,6 +15,7 @@ namespace PokemonApp
 {
     public partial class Form1 : Form
     {
+        private List<dominio.Pokemon> lista;
         public Form1()
         {
             InitializeComponent();
@@ -23,13 +24,13 @@ namespace PokemonApp
         private void Form1_Load(object sender, EventArgs e)
         {
             cargar();
-            btnModificar.Visible = false;
+            ComboBoxes();
         }
 
         public void cargar()
         {
             PokemonNegocio negocio = new PokemonNegocio();
-            List<dominio.Pokemon> lista = negocio.Listar();
+            lista = negocio.Listar();
 
             try
             {
@@ -45,17 +46,21 @@ namespace PokemonApp
             }
         }
 
+        private void ComboBoxes()
+        {
+            List<String>Campos = new List<String>() { "Número", "Nombre", "Descripcion", "Tipo"};
+            cmbCampo.DataSource = Campos;
+        }
+
         private void dgvPokemon_SelectionChanged(object sender, EventArgs e)
         {
-            
-            dominio.Pokemon seleccionado = (dominio.Pokemon)dgvPokemon.CurrentRow.DataBoundItem;
-            LoadImage(seleccionado.UrlImagen);
-            
-            if(seleccionado != null)
-            {
-            btnModificar.Visible = true;
-            }
 
+            dominio.Pokemon seleccionado = (dominio.Pokemon)dgvPokemon.CurrentRow.DataBoundItem;
+
+            if (seleccionado != null)
+                LoadImage(seleccionado.UrlImagen);
+            else
+                MessageBox.Show("Debe seleccionar uno para su modificación");
         }
 
         private void LoadImage(string image)
@@ -92,22 +97,145 @@ namespace PokemonApp
         {
             //Guardamos los datos de la fila seleccionada
             dominio.Pokemon seleccionado = (dominio.Pokemon)dgvPokemon.CurrentRow.DataBoundItem;
-
-            //Listamos todos para poder buscar el elegido
-            PokemonNegocio negocio = new PokemonNegocio();
-            List<dominio.Pokemon> lista = negocio.Listar();
-            
-            //Filtramos el Pokemon que necesitamos
-            dominio.Pokemon aModificar = lista.Find(J => J.Id == seleccionado.Id);
             
             //Creamos nueva ventana
-            frmAltaPokemon modificar = new frmAltaPokemon(aModificar);
+            frmAltaPokemon modificar = new frmAltaPokemon(seleccionado);
 
             //Mostramos ventana
             modificar.ShowDialog();
 
             //Cargamos Grid en caso de actualizaciones
             cargar();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+
+        private void btnBorrarLogica_Click(object sender, EventArgs e)
+        {
+            Eliminar(true);
+        }
+
+        private void Eliminar(bool logico = false)
+        {
+            dominio.Pokemon seleccionado = (dominio.Pokemon)dgvPokemon.CurrentRow.DataBoundItem;
+            PokemonNegocio pokemonNegocio = new PokemonNegocio();
+
+            //Mensaje previo a la eliminación
+            MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+            DialogResult result = MessageBox.Show("¿Está seguro que desea eliminar el Pokemon " + seleccionado.Nombre + "?", "Eliminar Pokemon", buttons, icon);
+
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    int rows = 0;
+                    
+                    if(logico)
+                    {
+                        rows = pokemonNegocio.EliminarLogica(seleccionado.Id);
+                    }
+                    else
+                    {
+                        rows = pokemonNegocio.Eliminar(seleccionado.Id);
+                    }
+
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Se ha eliminado el registro");
+                        cargar();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se ha podido eliminar el registro");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cargar();
+                }
+
+            }
+        }
+
+        private void btnBusqueda_Click(object sender, EventArgs e)
+        {
+            PokemonNegocio pokemonNegocio = new PokemonNegocio();
+
+            try
+            {
+                string busqueda = txtBusquedaAvanzada.Text;
+                string campo = cmbCampo.SelectedItem.ToString();
+                string criterio = cmbCriterio.SelectedItem.ToString();
+                dgvPokemon.DataSource = pokemonNegocio.BusquedaAvanzada(busqueda, campo, criterio);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void txtBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            busqueda();
+        }
+
+        private void busqueda()
+        {
+            string busqueda = txtBusqueda.Text;
+
+            if (string.IsNullOrEmpty(busqueda))
+            {
+                dgvPokemon.DataSource = lista;
+            }
+            else
+            {
+                try
+                {
+                    var pokemonEncontrado = lista.FindAll(pokemon => pokemon.Nombre.ToLower().Contains(busqueda.ToLower()) || pokemon.Tipo.Descripcion.ToLower().Contains(busqueda.ToLower()));
+
+                    if (pokemonEncontrado != null)
+                    {
+                        dgvPokemon.DataSource = pokemonEncontrado;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El pókemon no fue encontrado");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Pokemon no encontrado " + ex.Message);
+                }
+            }
+        }
+
+        private void cmbCampo_SelectedIndexchanged(object sender, EventArgs e)
+        {
+
+            List<String> Numerico = new List<String>() { "Menor a", "Mayor a", "Igual a" };
+            List<String> Nombre = new List<String>() { "Comienza con", "Termina con", "Contiene" };
+
+            if (cmbCampo.SelectedItem.ToString() == "Número")
+            {
+                cmbCriterio.DataSource = Numerico;
+            }
+            else
+            {
+                cmbCriterio.DataSource = Nombre;
+            }
+
         }
     }
 }
